09d04d1fdfa087fe8a0df8a9c3ec9b62a48261c3
vomanhtuong1 <manhtuongat@gmail.com>
2024-08-10 19:05:49 +0700

    add

--- web-decore/JS-decore/print_form_sign_up.js
+++ web-decore/JS-decore/print_form_sign_up.js
@@ -1,14 +1,15 @@
 const choose_sign_up = document.querySelector('.up')
-choose_sign_up.onclick = function() {
+console.log(choose_sign_up);
+
+choose_sign_up.onclick = function () {
     const sign_up = document.getElementById('sign_up')
     const container_all = document.getElementById('container-all')
     sign_up.style.display = 'block'
     container_all.style.filter = 'brightness(50%)'
     const form_sign_up = document.querySelector('#form_sign_up')
     const choose_x = document.querySelector('.myicon_sign_up')
-    console.log(choose_x)
-    console.log(form_sign_up)
-    choose_x.onclick = function closeForm_sign_up(){
+    const choose_btn_sign_up = document.querySelector('.btn_sign_up')
+    choose_x.onclick = function closeForm_sign_up() {
         form_sign_up.style.display = 'none'
         container_all.style.filter = 'none'
         location.reload()
